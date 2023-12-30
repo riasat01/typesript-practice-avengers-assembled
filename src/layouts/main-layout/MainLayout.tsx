1f975d20bc3ff8bf473e63729ae8f99f5d8f734c
unknown <riasat01@outlook.com>
2023-12-30 21:18:00 +0600

    navbar added

--- src/layouts/main-layout/MainLayout.tsx
+++ src/layouts/main-layout/MainLayout.tsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Navbar from '../../shared-components/main-layout/navbar/Navbar'
 
 
 const MainLayout = () => {
     return (
         <div>
-            This is main
+            <Navbar />
             <Outlet />
         </div>
     );
